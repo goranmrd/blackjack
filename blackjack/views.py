4a8d65b4388b0647a964e01fd5d2a3786b16502b
Goran Lazarevski <goranmrd@gmail.com>
2018-07-25 16:18:04 +0200

    Blackjack urls created and blank views

--- blackjack/views.py
+++ blackjack/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    return render(request)
+  
+def SignUp(request):
+    return render(request)
 
+def update_game(request):
+    return render(request)

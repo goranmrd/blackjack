e6d2370be101e896586af5a8b4e20e21331d475f
Goran Lazarevski <goranmrd@gmail.com>
2018-07-25 16:13:44 +0200

    Base urls added

--- game/urls.py
+++ game/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.Home, name='home'),
+    path('blackjack/', include('blackjack.urls')),
+    path('thanks/',views.ThanksPage.as_view(),name='thanks'),
     path('admin/', admin.site.urls),
 ]
